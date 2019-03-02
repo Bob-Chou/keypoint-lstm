675e88bcfedf75f0abf4dbddc4f05b5052572243
Bob <me.bozhou@gmail.com>
2019-03-02 16:55:26 +0800

    support config
    support lstm with mask

--- st_lstm.py
+++ st_lstm.py
@@ -9,109 +9,178 @@ class STModel(object):
     """docstring for STModel"""
     # protobuf
 
-    def __init__(self, n_frame, n_keypoint, n_dim=2, n_hidden=128, n_class=2):
+    def __init__(self, config):
         super(STModel, self).__init__()
+        self.name = config.name
         # network architecture
         self.graph = tf.Graph()
         self.x = None
         self.y = None
         self.y_one_hot = None
         self.keep_prob = None
-        self.n_keypoint = n_keypoint
-        self.n_frame = n_frame
-        self.n_dim = n_dim
-        self.n_hidden = n_hidden
-        self.n_class = n_class
-        self.lstm_outputs = []
+        self.n_keypoint = config.n_keypoint
+        self.n_frame = config.n_frame
+        self.n_dim = config.n_dim
+        self.n_hidden = config.n_hidden
+        self.n_class = config.n_class
+        self.logits = None
         self.pred = None
         # keep prob
         self.keep_prob = 0.5
         # loss
-        self.lambda1 = 1e-3
-        self.lambda3 = 5e-4
+        self.lambda1 = config.lambda1
+        self.lambda2 = config.lambda2
+        self.lambda3 = config.lambda3
         self.loss = None
         self.l0 = None
         self.l1 = None
         # train
-        self.lr = 1e-2
+        self.lr = config.learning_rate
         self.step = None
         # summary
-        self.summary = None
+        self.train_summary = None
+        self.test_summary = None
         self.accuracy = None
         self.saver = None
-        if tf.gfile.Exists('./summary'):
-            tf.gfile.DeleteRecursively('./summary')
-        tf.gfile.MakeDirs('./summary/train')
-        tf.gfile.MakeDirs('./summary/test')
-        self.train_writter = tf.summary.FileWriter('./summary/train')
-        self.test_writter = tf.summary.FileWriter('./summary/test')
+        summary_dir = "./summary/{}".format(self.name)
+        if tf.gfile.Exists(summary_dir):
+            tf.gfile.DeleteRecursively(summary_dir)
+        tf.gfile.MakeDirs(os.path.join(summary_dir, "train"))
+        tf.gfile.MakeDirs(os.path.join(summary_dir, "test"))
+        self.train_writter = tf.summary.FileWriter(
+            os.path.join(summary_dir, "train"))
+        self.test_writter = tf.summary.FileWriter(
+            os.path.join(summary_dir, "test"))
         # construct the network
         self._build_network()
 
     def _build_network(self):
-
+        train_summaries = []
+        test_summaries = []
         with self.graph.as_default():
-
-            # regularizer
-            with tf.variable_scope("l1_regularizer"):
-                reg1 = tf.contrib.layers.l2_regularizer(scale=self.lambda1)
-                reg3 = tf.contrib.layers.l1_regularizer(scale=self.lambda3)
 
             # input layer
             with tf.variable_scope("input"):
                 self.x = tf.placeholder(tf.float32, shape=(None, self.n_frame,
                                         self.n_keypoint, self.n_dim),
-                                        name="keypoint_holder")
+                                        name="feature")
                 self.y = tf.placeholder(tf.int32, shape=(None,),
-                                        name="label_holder")
-                self.batch = tf.placeholder(tf.int32, (), name="batch_size")
+                                        name="label")
+                self.mask = tf.placeholder(tf.int32, shape=(None,),
+                                           name="mask")
+                self.batch = tf.placeholder(tf.int32, [], name="batch_size")
                 # self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
                 self.y_one_hot = tf.one_hot(self.y, self.n_class)
 
+            # regularizer
+            with tf.variable_scope("l1_regularizer"):
+                reg1 = tf.contrib.layers.l2_regularizer(
+                    scale=self.lambda1 / tf.cast(self.batch, tf.float32))
+                reg2 = tf.contrib.layers.l2_regularizer(
+                    scale=self.lambda2 / tf.cast(self.batch, tf.float32))
+                reg3 = tf.contrib.layers.l1_regularizer(scale=self.lambda3)
+
+            # convert mask
+            mask_mat = tf.expand_dims(
+                tf.cast(tf.sequence_mask(self.mask, self.n_frame), tf.float32),
+                axis=-1, name="mask")
+            frame_len_mat = tf.expand_dims(
+                tf.cast(self.mask, tf.float32), axis=-1, name="length")
+
             # lstm
             with tf.variable_scope("lstm"):
-                n_input_dim = self.n_dim * self.n_keypoint
-                lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=n_input_dim,
-                                                    forget_bias=1.0,
-                                                    name="basic_lstm_cell",
-                                                    state_is_tuple=True)
+                lstm_cell = tf.nn.rnn_cell.LSTMCell(self.n_hidden,
+                                                    name="lstm_cell",
+                                                    dtype=tf.float32)
+                state = lstm_cell.zero_state(self.batch, dtype=tf.float32)
 
-                lstm_cell = tf.contrib.rnn.DropoutWrapper(cell=lstm_cell,
-                                                          input_keep_prob=1.0,
-                                                          output_keep_prob=self.keep_prob)
+            # run lstm through time steps manually
+            s_attns = []
+            t_attns = []
+            lstm_outputs = []
 
-                init_state = lstm_cell.zero_state(self.batch, dtype=tf.float32)
-                xt = tf.reshape(self.x,
-                                (-1, self.n_frame, self.n_dim *
-                                 self.n_keypoint))
-                outputs, _ = tf.nn.dynamic_rnn(lstm_cell, inputs=xt,
-                                               initial_state=init_state,
-                                               time_major=False)
+            for i in range(self.n_frame):
+                reuse = i != 0
 
+                xt = tf.reshape(self.x[:, i, :, :],
+                                (-1, self.n_dim * self.n_keypoint))
 
+                # spatio-attention
+                with tf.variable_scope("spatio_attention"):
+                    s_attn = attn_dense(xt, state.h, self.n_keypoint,
+                                        name="attention_dense",
+                                        reuse=reuse, regularizer=reg3,
+                                        rescale=True)
+                    s_attn = tf.nn.softmax(s_attn, axis=-1)
+                    # record for regularizer
+                    s_attns.append(s_attn)
+                    s_attn = tf.expand_dims(s_attn, -1)
+
+                # temporal-attention
+                with tf.variable_scope("temporal_attention"):
+                    t_attn = attn_dense(xt, state.h, 1, name="attention_dense",
+                                        activation=tf.nn.relu,
+                                        reuse=reuse, regularizer=reg3)
+                    # record for regularizer
+                    t_attns.append(t_attn)
+
+                with tf.variable_scope("spatio_attention"):
+                    # add spatio-attention to input sequence
+                    xt = s_attn * tf.reshape(xt,
+                                             (-1, self.n_keypoint, self.n_dim))
+                    xt = tf.reshape(xt, (-1, self.n_dim * self.n_keypoint))
+
+                # run lstm for a single timestep
+                out, state = lstm_cell(xt, state)
+
+                # add temporal-attention to output
+                with tf.variable_scope("temporal_attention"):
+                    out = dense(out, self.n_class, name="output_dense",
+                                activation=None, reuse=reuse,
+                                regularizer=reg3)
+
+                lstm_outputs.append(out * t_attn)
+
+            # stack the tensor
+            s_attn_mat = tf.stack(s_attns, axis=1,
+                                  name="spatio_attention_stack")
+            t_attn_mat = tf.stack(t_attns, axis=1,
+                                  name="temporal_attention_stack")
+            out_mat = tf.stack(lstm_outputs, axis=1, name="output_stack")
 
             # output
             with tf.variable_scope("output"):
-                logits = dense(outputs[:, -1, :], self.n_class, name="output_dense",
-                               activation=None, reuse=tf.AUTO_REUSE)
-                self.pred = tf.nn.softmax(logits, axis=-1)
+                self.logits = tf.reduce_sum(out_mat * mask_mat,
+                                            axis=1) / frame_len_mat
+                self.pred = tf.nn.softmax(self.logits, axis=-1)
                 correct = tf.equal(tf.argmax(self.pred, -1),
                                    tf.argmax(self.y_one_hot, -1))
                 self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-                tf.summary.scalar('accuracy', self.accuracy)
+
+                add_summary([tf.summary.scalar('accuracy', self.accuracy)],
+                            [train_summaries, test_summaries])
 
             # loss
             with tf.variable_scope("loss"):
                 # cross-entropy
-                self.l0 = tf.losses.softmax_cross_entropy(self.y_one_hot, logits)
+                self.l0 = tf.losses.softmax_cross_entropy(self.y_one_hot,
+                                                          self.logits)
                 # spatio-attention l2 regularization
-                # s_reg = 1 - self.n_keypoint * tf.add_n(s_attns) / self.n_frame
-                # self.l1 = tf.contrib.layers.apply_regularization(reg1, [s_reg])
+                s_reg = tf.reduce_sum(self.n_keypoint * s_attn_mat * mask_mat,
+                                      axis=1) / frame_len_mat - 1
+                self.l1 = tf.contrib.layers.apply_regularization(reg1, [s_reg])
+                # temporal-attention l2 regularization
+                t_reg = tf.reduce_sum(t_attn_mat * mask_mat,
+                                      axis=-1) / tf.math.sqrt(frame_len_mat)
+                self.l2 = tf.contrib.layers.apply_regularization(reg2, [t_reg])
                 # total loss
                 self.loss = tf.losses.get_total_loss()
-                tf.summary.scalar('loss', self.loss)
-                tf.summary.scalar('loss0', self.l0)
-                # tf.summary.scalar('loss1', self.l1)
+                add_summary(
+                    [tf.summary.scalar('loss: total', self.loss),
+                     tf.summary.scalar('loss: cross-entropy', self.l0),
+                     tf.summary.scalar('loss: spatio-attention', self.l1),
+                     tf.summary.scalar('loss: temporal-attention', self.l2)],
+                    [train_summaries])
 
             # optimizer
             with tf.variable_scope("optimizer"):
@@ -122,71 +191,87 @@ class STModel(object):
                                                global_step=self.global_step)
 
             # merged summary
-            self.summary = tf.summary.merge_all()
+            self.train_summary = tf.summary.merge(train_summaries)
+            self.test_summary = tf.summary.merge(test_summaries)
             self.saver = tf.train.Saver()
-            # [print(v) for v in tf.trainable_variables()]
+            # print("=================Architecture=================")
+            # for n in self.graph.as_graph_def().node:
+            #     print(n.name)
+            print("================Trainable Vars================")
+            for v in tf.trainable_variables():
+                print(v)
+            print("==============================================")
 
-    def train(self, train_data, valid_data=None, selected_list=None):
+    def train(self, train_data, valid_data=None, selected_list=None,
+              valid_per=100, save_per=1000):
+        ckpt_dir = "./checkpoint/{}".format(self.name)
+        if tf.gfile.Exists(ckpt_dir):
+            tf.gfile.DeleteRecursively(ckpt_dir)
+
         gpu_options = tf.GPUOptions(allow_growth=True,
                                     per_process_gpu_memory_fraction=0.7)
         with tf.Session(graph=self.graph,
                         config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
             sess.run(tf.global_variables_initializer())
-            for x, y in train_data:
+            tf.logging.info("Please monitor the training process with:\n"
+                            "tensorboard --logdir=./summary")
+            for (x, y), m in train_data:
                 if selected_list is not None:
                     x = x[:, :, selected_list, :]
                 curr_step = sess.run(self.global_step)
-                # assert x.shape[0] == y.shape[0]
-                summary, _ = sess.run([self.summary, self.step],
+                summary, _ = sess.run([self.train_summary, self.step],
                                       feed_dict={self.x: x,
                                                  self.y: y,
-                                                 self.batch: x.shape[0]})
+                                                 self.batch: x.shape[0],
+                                                 self.mask: m})
                 self.train_writter.add_summary(summary, curr_step)
-                if curr_step % 100 == 0 and valid_data is not None:
-                    _x, _y = valid_data
-                    summary, = sess.run([self.summary],
+                if curr_step % valid_per == 0 and valid_data is not None:
+                    _x, _y, _m = valid_data
+                    summary, = sess.run([self.test_summary],
                                         feed_dict={self.x: _x,
                                                    self.y: _y,
-                                                   self.batch: _x.shape[0]})
+                                                   self.batch: _x.shape[0],
+                                                   self.mask: _m})
                     self.test_writter.add_summary(summary, curr_step)
-                if curr_step % 1000 == 0:
-                    self.saver.save(sess, "./checkpoint/st_lstm.ckpt",
+                if curr_step % save_per == 0:
+                    self.saver.save(sess, os.path.join(ckpt_dir, "model.ckpt"),
                                     global_step=curr_step)
 
 
 if __name__ == "__main__":
-    n_frame = 10
-    n_keypoint = 30
-    n_dim = 3
-    batch_size = 16
-    max_epoch = 10000
-    n_class = 8
-
-    root = "/home/bob/Workspace/undergrad-project/dataset/SBU"
-    train_set, valid_set = walk_dataset(root, ".txt", valid_list=["s05"])
-    train_label = [int(sample.split("/")[-3]) - 1 for sample in train_set]
-    valid_label = [int(sample.split("/")[-3]) - 1 for sample in valid_set]
-    valid_set = load_batch_parallel(valid_set, [n_frame, n_keypoint, n_dim])
-    valid_set = (valid_set - [0.5, 0.5, 1]) / [0.5, 0.5, 1.]
+    config = get_config("./config.ini")
 
     def training_data_generator(package, shape, batch_size, max_step=None,
                                 max_epoch=None):
         for batch in random_batch_generator(package, batch_size,
                                             max_step=max_step,
                                             max_epoch=max_epoch):
-            batch[0] = load_batch_parallel(batch[0], shape)
-            batch[0] = (batch[0] - [0.5, 0.5, 1]) / [0.5, 0.5, 1.]
-            yield batch
+            batch[0], mask = load_batch_parallel(batch[0], shape)
+            batch[0] = (batch[0] - config.data.mean) / config.data.std
+            yield batch, np.sum(mask != -1, axis=-1)
+
+    model = STModel(config.model)
+
+    train_set, valid_set = walk_dataset(config.data.root, config.data.ext,
+                                        valid_list=config.train.valid_set)
+    train_label = [int(sample.split("/")[-3]) - 1 for sample in train_set]
+    valid_label = [int(sample.split("/")[-3]) - 1 for sample in valid_set]
+    valid_set, valid_mask = load_batch_parallel(valid_set,
+                                          [config.model.n_frame,
+                                           config.model.n_keypoint,
+                                           config.model.n_dim])
+    valid_set = (valid_set - config.data.mean) / config.data.std
 
     training_data = training_data_generator([train_set, train_label],
-                                            [n_frame, n_keypoint, n_dim],
-                                            batch_size,
-                                            max_epoch=max_epoch)
+                                            [config.model.n_frame,
+                                             config.model.n_keypoint,
+                                             config.model.n_dim],
+                                            config.train.batch_size,
+                                            max_epoch=config.train.n_epoch)
 
-    model = STModel(n_frame, n_keypoint, n_dim=n_dim, n_hidden=64,
-                    n_class=n_class)
-    model.train(training_data, valid_data=[valid_set, valid_label])
-
-
-
-
+    model.train(training_data,
+                valid_data=[valid_set,
+                            valid_label,
+                            np.sum(valid_mask != -1, axis=-1)],
+                valid_per=config.train.valid_per_step,
+                save_per=config.train.save_per_step)
--- utils.py
+++ utils.py
@@ -3,18 +3,84 @@ import os
 import numpy as np
 import threading
 import tensorflow as tf
+from configparser import ConfigParser
 
 
 def dense(x, z_dim, name, activation=None, reuse=None, regularizer=None):
-    """Normal dense layer"""
+    """Normal dense layer
+
+    :param x: input tensor
+    :param z_dim: output dimension
+    :param name: name of operation
+    :param activation: activation function
+    :param reuse: reuse tf.Variable
+    :param regularizer: regularizer of parameters
+    :return: the output of dense layer
+    """
     x_dim = x.shape[-1]
     with tf.variable_scope(name, reuse=reuse):
         w = tf.get_variable("w", (x_dim, z_dim), regularizer=regularizer)
-        b = tf.get_variable("b", z_dim)
-        z = tf.add(tf.matmul(x, w), b)
+        b = tf.get_variable("b", 1)
+        z = tf.matmul(x, w) + b
         if activation is not None:
             z = activation(z)
     return z
+
+
+def attn_dense(x, h, attn_dim, name, activation=tf.nn.tanh, reuse=False,
+               regularizer=None, rescale=False):
+    """Spatio attention
+
+    :param x: input x
+    :param h: input h from lstm
+    :param attn_dim: dimension of attention
+    :param name: name of operation
+    :param activation: activation function
+    :param reuse: reuse tf.Variable
+    :param regularizer: regularizer of parameters
+    :param rescale: whether to rescale the attention
+    :return: attention tensor
+    """
+    x_dim = x.shape[-1]
+    h_dim = h.shape[-1]
+    with tf.variable_scope(name, reuse=reuse):
+        wxs = tf.get_variable("wxs", (x_dim, attn_dim), regularizer=regularizer)
+        whs = tf.get_variable("whs", (h_dim, attn_dim), regularizer=regularizer)
+        bs = tf.get_variable("bs", 1)
+        attn = tf.matmul(x, wxs) + tf.matmul(h, whs) + bs
+
+        if rescale:
+            us = tf.get_variable("us", 1, regularizer=regularizer)
+            bus = tf.get_variable("bus", 1, regularizer=regularizer)
+        else:
+            us = 1
+            bus = 0
+
+        if activation is not None:
+            attn = activation(attn)
+
+    return us * attn + bus
+
+
+def add_summary(summary_list, collection_list):
+    """Add summary to assigned collection
+
+    :param summary_list: a list of tf summary object
+    :param collection_list: a list contains all the collection for all summary
+                            objects to be added
+    :return: None
+    """
+    list(map(lambda x: x.extend(summary_list), collection_list))
+
+
+def filter_trainable_vars(var_list, freeze_list=[]):
+    """Return the filtered variable list
+
+    :param var_list: all variable list
+    :param freeze_list: key words for variables to be left out
+    :return:
+    """
+    return [v for v in var_list if all([k not in v for k in freeze_list])]
 
 
 def random_batch_generator(package, batch_size, max_step=None, max_epoch=None):
@@ -74,35 +140,44 @@ def walk_dataset(root, data_ext, valid_list=[]):
     return dataset[0], dataset[1]
 
 
-def load_batch_parallel(file_list, shape):
+def load_batch_parallel(file_list, shape, as_flow=False):
     """Load skeleton data vector from disk parrallel
 
     :param file_list: a list contains all the file path to be loaded
     :param shape: resized shape in [n_frame, n_point, n_dim], e.g. [10, 30, 3]
+    :param as_flow: whether to return the flow formatted vector
     :return: a np.darray with the assigned shape
     """
     if len(file_list) == 0:
         return None
 
-    def _load_data_load_data_parallel(file, idx, shape, out):
+    def _load_data_parallel(file, idx, shape, out):
         n_frame = shape[0]
         with open(file) as f:
             data_array = [list(map(float, line.strip().split(",")))
                           for line in f.readlines()]
             data_array = np.reshape(np.asarray(data_array)[:, 1:],
                                     [-1, shape[1], shape[2]])
-        skip_length = data_array.shape[0] // n_frame
-        equal_length = skip_length * n_frame
-        start = np.random.randint(data_array.shape[0] - equal_length
-                                  + skip_length)
-        skip_idxs = [start + i * skip_length for i in range(n_frame)]
-        out[idx] = (data_array[skip_idxs], skip_idxs)
+        real_length = data_array.shape[0]
+        if real_length >= n_frame:
+            skip_length = data_array.shape[0] // n_frame
+            equal_length = skip_length * n_frame
+            start = np.random.randint(data_array.shape[0] - equal_length
+                                      + skip_length)
+            selected_idxs = [start + i * skip_length for i in range(n_frame)]
+        else:
+            data_array = np.pad(data_array,
+                                [[0, n_frame-real_length], [0, 0], [0, 0]],
+                                "mean")
+            selected_idxs = list(range(real_length)) + \
+                            [-1 for _ in range(n_frame - real_length)]
+        out[idx] = (data_array[selected_idxs], selected_idxs)
 
     collection = list(range(len(file_list)))
     threads = []
     for i, file in enumerate(file_list):
         thread = threading.Thread(
-            target=_load_data_load_data_parallel,
+            target=_load_data_parallel,
             args=(file, i, shape, collection))
         thread.start()
         threads.append(thread)
@@ -111,8 +186,53 @@ def load_batch_parallel(file_list, shape):
         th.join()
     threads.clear()
 
-    batch, skip_idxs = list(zip(*collection))
+    batch, idxs = list(zip(*collection))
 
-    return np.asarray(batch)
+    if as_flow:
+        batch = batch[1:] - batch[:-1]
+        idxs = idxs[:-1]
+
+    return np.asarray(batch), np.asarray(idxs)
 
 
+def get_config(filename):
+    """Read and parse the config file
+
+    :param filename: path to the config file
+    :return: configure
+    """
+    class Config(object):
+        pass
+
+    reader = ConfigParser()
+    reader.read(os.path.expanduser(filename))
+    config = Config()
+
+    config.data = Config()
+    config.data.root = reader.get("data", "root")
+    config.data.ext = reader.get("data", "ext")
+    config.data.as_flow = reader.getboolean("data", "as_flow")
+    config.data.mean = list(map(float,
+                                reader.get("data", "mean").split(",")))
+    config.data.std = list(map(float,
+                               reader.get("data", "std").split(",")))
+    config.model = Config()
+    config.model.name = reader.get("model", "name")
+    config.model.n_frame = reader.getint("model", "n_frame")
+    config.model.n_class = reader.getint("model", "n_class")
+    config.model.n_keypoint = reader.getint("model", "n_keypoint")
+    config.model.n_dim = reader.getint("model", "n_dim")
+    config.model.n_hidden = reader.getint("model", "n_hidden")
+    config.model.learning_rate = reader.getfloat("model", "learning_rate")
+    config.model.lambda1 = reader.getfloat("model", "lambda1")
+    config.model.lambda2 = reader.getfloat("model", "lambda2")
+    config.model.lambda3 = reader.getfloat("model", "lambda3")
+
+    config.train = Config()
+    config.train.batch_size = reader.getint("train", "batch_size")
+    config.train.n_epoch = reader.getint("train", "n_epoch")
+    config.train.valid_set = reader.get("train", "valid_set").split(",")
+    config.train.valid_per_step = reader.getint("train", "valid_per_step")
+    config.train.save_per_step = reader.getint("train", "save_per_step")
+
+    return config
